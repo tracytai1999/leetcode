7d2610b07957210f083cb9171a6c268e464ad8fb
Tracy Tai <tracytai1999@gmail.com>
2022-04-07 22:38:44 -0700

    Daily Temperatures

---
+++ Stack/739.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int temp = temperatures[i];
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temp) {
+                int prev = stack.pop();
+                res[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
