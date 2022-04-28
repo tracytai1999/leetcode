9abff62065ce102c494c83c8c5b72fabd26f7ba1
Tracy Tai <tracytai1999@gmail.com>
2022-04-27 20:34:30 -0700

    Longest Increasing Subsequence

---
+++ 300.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int max = 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        for (int c: dp) {
+            max = Math.max(max, c);
+        }
+        return max;
+    }
+}
