058eb7896e15f69a73fad86be1c2058a055b465a
Tracy Tai <tracytai1999@gmail.com>
2022-05-01 21:15:29 -0700

    Balanced Binary Tree

---
+++ 110.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+    
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+        
+        int left = getHeight(node.left);
+        int right = getHeight(node.right);
+        
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1){
+            return -1;
+        }
+        
+        return Math.max(left, right) + 1;
+    } 
+    
+}
