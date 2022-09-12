e952c7e25ae02d8055895fd565a28f9d133c4bc1
Tracy Tai <tracytai1999@gmail.com>
2022-09-12 14:00:13 -0700

    Palindrome Linked List

---
+++ 234.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+        
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+        
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
