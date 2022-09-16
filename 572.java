3548462fcd6638b8905dabe841bbd99988350bfb
Tracy Tai <tracytai1999@gmail.com>
2022-09-15 19:00:36 -0700

    Subtree of Another Tree

---
+++ 572.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null)
+            return false;
+        
+        if(root.val == subRoot.val){
+            if(isSameTree(root, subRoot))
+                return true;
+        }
+        
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);  
+    }
+    
+    private boolean isSameTree(TreeNode p, TreeNode q){
+        if(p == null && q == null)
+            return true;
+        if(p == null || q == null || p.val != q.val)
+            return false;
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
