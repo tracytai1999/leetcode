55a0e645a825c2d784d7d0ffc03b5b2f65acd960
Tracy Tai <tracytai1999@gmail.com>
2022-10-19 15:29:56 -0700

    Make Array Zero by Subtracting Equal Amounts

---
+++ 2357.java
@@ -0,0 +1,7 @@
+public int minimumOperations(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int a: nums)
+            if (a > 0)
+                set.add(a);
+        return set.size();
+    }
