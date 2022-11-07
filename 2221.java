889cc27a02afc7065ac14ebf57b5c8d80373fcc2
Tracy Tai <tracytai1999@gmail.com>
2022-11-06 17:21:13 -0800

    Find Triangular Sum of an Array

---
+++ 2221.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        return dfs(nums);
+    }
+    
+    public int dfs(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int[] temp = new int[nums.length - 1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        return dfs(temp);
+    }
+}
