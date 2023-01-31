17375ddde10770012c76f0a5eae833d4f16a4b0e
Tracy Tai <tai.zi@northeastern.edu>
2023-01-30 18:12:30 -0800

    Reverse Bits

---
+++ 190.java
@@ -0,0 +1,12 @@
+class Solution {
+      public int reverseBits(int n) {
+        int results = 0;
+        for (int i = 0; i < 32; i++) {
+            int end = n & 1;
+            n >>= 1;
+            results <<= 1;
+            results |= end;
+        }
+        return results;
+    }
+}
