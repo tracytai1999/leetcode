84d12c81b7b3970dff3c60bb93ce0143b9b892cd
Tracy Tai <tai.zi@northeastern.edu>
2023-02-17 00:01:20 -0800

    4Sum II

---
+++ 454.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int cnt = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int a : A) {
+            for (int b : B) {
+                m.put(a + b, m.getOrDefault(a + b, 0) + 1);
+            }
+        }
+        for (int c : C) {
+            for (int d : D) {
+                cnt += m.getOrDefault(-(c + d), 0);
+            }
+        }
+        return cnt;
+    }
+}
