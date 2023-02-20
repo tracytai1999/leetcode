0327d59135bb53ac8970016be0fe3b1a318583b5
Tracy Tai <tai.zi@northeastern.edu>
2023-02-19 20:21:44 -0800

    Subsets

--- 78.java
+++ 78.java
@@ -15,3 +15,20 @@ class Solution {
     return output;
   }
 }
+
+class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    dfs(nums, 0, res, new ArrayList<>());
+    return res;
+  }
+
+  private void dfs(int[] nums, int index, List<List<Integer>> res, List<Integer> list) {
+      res.add(new ArrayList<>(list));
+      for (int i = index; i < nums.length; i++) {
+          list.add(nums[i]);
+          dfs(nums, i + 1, res, list);
+          list.remove(list.size() - 1);
+      }
+  }
+}
