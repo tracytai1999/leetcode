a737f72273bb228ed6fb94f6bfa2035aaa0c4abf
Tracy Tai <tai.zi@northeastern.edu>
2023-02-28 21:08:55 -0800

    Contains Duplicate III

---
+++ Array/220.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = Math.max(i - k, 0); j < i; ++j) {
+                if (Math.abs((long) nums[i] - nums[j]) <= t) return true;
+            }
+        }
+        return false;
+    }
+}
