74c70bd76e1d1828aa06a9dc01d26b514d6aa2a7
Tracy Tai <tai.zi@northeastern.edu>
2023-07-06 21:45:36 -0700

    Merge Strings Alternately

--- 1768.java
+++ 1768.java
@@ -34,3 +34,22 @@ class Solution {
         return res.toString();
     }
 }
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < Math.max(m, n); i++) {
+            if (i < m) {
+                result.append(word1.charAt(i));
+            }
+            if (i < n) {
+                result.append(word2.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+}
