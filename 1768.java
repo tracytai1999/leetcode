4d1a3acdea843deb5a2ff2dba6ccfdb9a58cc62e
Tracy Tai <tai.zi@northeastern.edu>
2023-07-06 21:41:50 -0700

    Merge Strings Alternately

---
+++ 1768.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int length = Math.min(word1.length(), word2.length());
+        StringBuilder res = new StringBuilder();
+        int one = 1;
+        int two = 0;
+        if (length == 0) {
+            return word1 + word2;
+        }
+        for (int i = 0; i < length * 2; i++) {
+            if (i == 0) {
+                res.append(word1.charAt(i));
+            }else if (i % 2 == 0) {
+                res.append(word1.charAt(one));
+                one++;
+            }else if (i % 2 == 1) {
+                res.append(word2.charAt(two));
+                two++;
+            }
+        }
+        if (word1.length() > word2.length()){
+            for (int i = length * 2; i < word1.length() + word2.length(); i++) {
+                res.append(word1.charAt(one));
+                one++;
+            }
+        }
+        if (word1.length() < word2.length()){
+            for (int i = length * 2; i < word1.length() + word2.length(); i++) {
+                res.append(word2.charAt(one));
+                one++;
+            }
+        }
+        
+        return res.toString();
+    }
+}
